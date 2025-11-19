6404ea385b180553c45aad3873649f5ce0547d0a
ViliusP <viliuspaliokas@gmail.com>
2025-11-19 13:52:38 +0200

    Add more tests

--- src/tests/Vilip.Utilties.EnvironmentTransformer/VariableTransformers/EnvSnakeCaseToPascalTransformerTests.cs
+++ src/tests/Vilip.Utilties.EnvironmentTransformer/VariableTransformers/EnvSnakeCaseToPascalTransformerTests.cs
@@ -1,9 +1,9 @@
 using System.Globalization;
 using FluentAssertions;
 using Vilip.Utilties.EnvironmentTransformer.VariableTransformers;
-using Xunit;
 
-namespace Vilip.Utilties.EnvironmentTransformer.Tests;
+namespace Vilip.Utilties.EnvironmentTransformer.Tests.VariableTransformers;
+
 
 public class EnvSnakeCaseToPascalTransformerTests
 {
@@ -65,5 +65,80 @@ public class EnvSnakeCaseToPascalTransformerTests
         {
             CultureInfo.CurrentCulture = original;
         }
+    }
+
+    [Fact]
+    public void EmptyString_Returns_Empty()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null);
+        t.Apply("").Should().Be("");
+    }
+
+    [Fact]
+    public void SingleUnderscore_Treated_As_Separator()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null);
+        t.Apply("_leading").Should().Be("Leading");
+        t.Apply("trailing_").Should().Be("Trailing");
+        t.Apply("_both_").Should().Be("Both");
+    }
+
+    [Fact]
+    public void MultipleConsecutiveUnderscores_Are_Ignored()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null);
+        t.Apply("mqtt__client").Should().Be("Mqtt__Client");
+        t.Apply("mqtt___client").Should().Be("Mqtt__Client");
+    }
+
+    [Fact]
+    public void DoubleUnderscore_Splits_Sections_Correctly()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null);
+        t.Apply("mqtt__client__host").Should().Be("Mqtt__Client__Host");
+    }
+
+    [Fact]
+    public void MixedCase_Input_Normalized_To_Pascal()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null);
+        t.Apply("mQtT_cLiEnT").Should().Be("MqttClient");
+    }
+
+    [Fact]
+    public void Numbers_Are_Preserved()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null);
+        t.Apply("mqtt_client_123").Should().Be("MqttClient123");
+    }
+
+    [Fact]
+    public void SpecialCharacters_Remain_In_Words()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null);
+        t.Apply("mqtt_client_v2_beta").Should().Be("MqttClientV2Beta");
+    }
+
+    [Fact]
+    public void MatcherTrue_On_EmptyString_Transforms()
+    {
+        static bool m(string v) => string.IsNullOrEmpty(v);
+        var t = new EnvSnakeCaseToPascalTransformer(m);
+        t.Apply("").Should().Be(""); // Nothing to transform, but logic still runs
+    }
+
+    [Fact]
+    public void MatcherFalse_On_All_Skips_All()
+    {
+        static bool m(string v) => false;
+        var t = new EnvSnakeCaseToPascalTransformer(m);
+        t.Apply("mqtt_client").Should().Be("mqtt_client");
+    }
+
+    [Fact]
+    public void Invariant_TitleCase_Is_Stable_Across_Cultures()
+    {
+        var t = new EnvSnakeCaseToPascalTransformer(null, new CultureInfo("tr-TR"));
+        t.Apply("istanbul_api").Should().Be("İstanbulApi");
     }
 }
