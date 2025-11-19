77f9ace2518a660de4b05874c4891b59cf799f8d
ViliusP <viliuspaliokas@gmail.com>
2025-11-19 13:27:21 +0200

    Refactor EnvironmentTranfsormer

--- src/Vilip.Utilties.EnvironmentTransformer/EnvironmentTransformer.cs
+++ src/Vilip.Utilties.EnvironmentTransformer/EnvironmentTransformer.cs
@@ -5,7 +5,7 @@ using Vilip.Utilties.EnvironmentTransformer;
 
 class EnvironmentTransformer
 {
-    static void Apply(EnvironmentTransformerOptions options)
+    public static void Apply(EnvironmentTransformerOptions options)
     {
         var env = options.EnvironmentTarget is EnvironmentVariableTarget target
             ? Environment.GetEnvironmentVariables(target)
@@ -13,19 +13,16 @@ class EnvironmentTransformer
 
         foreach (DictionaryEntry entry in env)
         {
-            object objectKey = entry.Key;
-            if (objectKey is not string key) continue;
-
-            object? objectValue = entry.Value;
-            if (objectValue == null || objectValue is not string value) continue;
+            if (entry.Key is not string key) continue;
+            if (entry.Value is not string value) continue;
 
             string newKey = options.Transformer(key);
-            if(newKey == key) continue;
+            if (string.Equals(newKey, key, StringComparison.Ordinal)) continue;
 
-            Environment.SetEnvironmentVariable(newKey, value);
+            Environment.SetEnvironmentVariable(newKey, value, options.EnvironmentTarget ?? EnvironmentVariableTarget.Process);
             if (options.RemoveAfterTransform)
             {
-                Environment.SetEnvironmentVariable(key, null);
+                Environment.SetEnvironmentVariable(key, null, options.EnvironmentTarget ?? EnvironmentVariableTarget.Process);
             }
         }
     }
